f93bcafd7a335650d611fc6f562ebe79b0542253
Devisha <157816727+git-devisha@users.noreply.github.com>
2025-07-13 16:25:08 +0530

    initial commit

---
+++ insurance_sale/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+app= FastAPI()
+
+class InputData(BaseModel):
+    Gender: str
+    Age: int
+    HasDrvingLicense: int
+    RegionID: int
+    PastAccident : str
+    AnnualPremium: float
+
+model= joblib.load("/insurance_sale/models/model.pkl")
+@app.get("/")
+async def read_root():
+    return {"health check": "ok", "model_version": "v1.0"}
+
+@app.post("/insurance_sale/predict")
+async def predict(input_data: InputData):
+    df= pd.DataFrame([input_data.model_dump().values()], columns= input_data.model_dump().keys())
+    pred= model.prediction(df)
+    return {"prediction": int(pred[0])}
